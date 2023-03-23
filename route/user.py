07e100db5b87604308d94eeec7e4686c4485da9d
denizparlak1 <60672820+denizparlak1@users.noreply.github.com>
2023-03-23 18:11:59 +0300

    Kullanıcının güncelleme işlemleri eklendi

--- route/user.py
+++ route/user.py
@@ -1,9 +1,8 @@
-from io import BytesIO
-
 from fastapi import APIRouter, HTTPException, UploadFile
-from firebase_admin import db
 
+from firebase_admin import auth
 from auth.config import users_ref
+from schema.user.schema import UpdateUserMessage, UpdateUserPhone, UpdateUserPassword, UpdateUserEmail
 from storage.firebase_storage import upload_to_firebase_storage, upload_to_gcs
 
 router = APIRouter()
@@ -25,4 +24,43 @@ async def get_user(userId: str):
 @router.post("/users/add/avatar/{userId}")
 async def add_avatar_api(userId: str, file: UploadFile):
     url = f'user/{userId}/{file.filename}'
-    return upload_to_gcs(url,file.file)
+    return upload_to_gcs(url, file.file)
+
+
+@router.put("/user/update/message/")
+async def update_message_api(user: UpdateUserMessage):
+    try:
+        users_ref.child(user.user_id).update({"message": user.message})
+        return {"message": "Mesajınız Güncellendi"}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@router.put("/user/update/phone/")
+async def update_phone_api(user: UpdateUserPhone):
+    try:
+        users_ref.child(user.user_id).update({"phone": user.phone})
+        return {"message": "Telefon Numaranız Güncellendi"}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@router.put("/user/update/email/")
+async def update_email_api(user: UpdateUserEmail):
+    try:
+        # Update the user's email in Firebase Authentication
+        auth.update_user(user.user_id, email=user.email)
+        users_ref.child(user.user_id).update({"email": user.email})
+        return {"detail": "Email Güncellendi"}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@router.put("/user/update/password/")
+async def update_password_api(user: UpdateUserPassword):
+    try:
+        # Update the user's email in Firebase Authentication
+        auth.update_user(user.user_id, password=user.password)
+        return {"detail": "Şifre Güncellendi"}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
---
+++ schema/user/schema.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+
+
+class BaseUpdateUser(BaseModel):
+    user_id: str
+
+
+class UpdateUserPhone(BaseUpdateUser):
+    phone: str
+
+
+class UpdateUserMessage(BaseUpdateUser):
+    message: str
+
+
+class UpdateUserEmail(BaseUpdateUser):
+    email: str
+
+
+class UpdateUserPassword(BaseUpdateUser):
+    password: str
