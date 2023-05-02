af277e4a5d5d49daac33e5828b6ced07a4d0c9a0
denizparlak1 <60672820+denizparlak1@users.noreply.github.com>
2023-05-03 02:04:27 +0300

    Update config.py
    credentials storage'dan okuma sağlandı.

--- auth/config.py
+++ auth/config.py
@@ -11,28 +11,29 @@ database_url = os.environ['FIREBASE_DATABASE_URL']
 bucket_name = os.environ['BUCKET']
 corp_bucket_name = os.environ['CORP_BUCKET']
 
+def download_json_key(bucket_name, blob_path):
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(blob_path)
 
-storage_client = storage.Client()
-bucket = storage_client.get_bucket(bucket_name)
-blob = bucket.blob(firebase_json_path)
+    with tempfile.NamedTemporaryFile(mode="wb", delete=False, suffix=".json") as temp_credentials_file:
+        blob.download_to_file(temp_credentials_file)
+        temp_file_path = temp_credentials_file.name
 
-# Download firebase json key
-#with tempfile.NamedTemporaryFile(mode="wb", delete=False) as temp_credentials_file:
-#    blob.download_to_file(temp_credentials_file)
-#    temp_file_path = temp_credentials_file.name
-#
-cred = credentials.Certificate(firebase_json_path)
+    return temp_file_path
 
+firebase_key_path = download_json_key(bucket_name, firebase_json_path)
+
+cred = credentials.Certificate(firebase_key_path)
 initialize_app(cred, {
     'databaseURL': database_url
 })
 
-storage_client = storage.Client.from_service_account_json(firebase_json_path)
+storage_client = storage.Client.from_service_account_json(firebase_key_path)
 bucket = storage_client.get_bucket(bucket_name)
 corp_bucket = storage_client.get_bucket(corp_bucket_name)
 users_ref = db.reference('users')
 corp_ref = db.reference('corporate')
-
 
 def get_firestore_client():
     return firestore.client()
